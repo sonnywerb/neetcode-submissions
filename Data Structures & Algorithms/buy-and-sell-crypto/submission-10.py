9dbecd67494a74d6b77dbd0df92ee65ac0a94a9d
Sonny <67868864+sonnywerb@users.noreply.github.com>
2026-09-11 21:54:01 -0600

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        # sliding window solution
+        l, r = 0, 1
+        max_p = 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                max_p = max(max_p, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        return max_p
+        
+        # DP/greedy algorithm
+        # min_buy, max_p = prices[0], 0
+
+        # for i in range(len(prices)):
+        #     min_buy = min(min_buy, prices[i])
+        #     max_p = max(max_p, prices[i] - min_buy)
+            
+        # return max_p
+
+        
